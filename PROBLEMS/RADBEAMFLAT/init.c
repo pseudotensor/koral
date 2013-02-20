f360d194a87f52637f4854d49a550554ea470a8a
Olek <olek.sadowski@gmail.com>
2013-02-19 20:18:30 -0500

    Added RADBEAMFLAT files

---
+++ PROBLEMS/RADBEAMFLAT/init.c
@@ -0,0 +1,74 @@
+
+/*
+  int
+  set_initial_profile()
+  {
+  int ix,iy,iz;
+  for(iz=0;iz<NZ;iz++)
+  {
+  for(iy=0;iy<NY;iy++)
+  {
+  for(ix=0;ix<NX;ix++)
+  {
+*/
+
+ldouble rho,mx,my,mz,m,E,uint,E0,Fx,Fy,Fz,pLTE;  
+ldouble xx,yy,zz;
+ldouble uu[NV];
+
+xx=get_x(ix,0);
+yy=get_x(iy,1);
+zz=get_x(iz,2);
+ldouble gg[4][5],GG[4][5],tup[4][4],tlo[4][4],eup[4][4],elo[4][4];
+pick_g(ix,iy,iz,gg);
+pick_G(ix,iy,iz,GG);
+pick_T(tmuup,ix,iy,iz,tup);
+pick_T(tmulo,ix,iy,iz,tlo);
+pick_T(emuup,ix,iy,iz,eup);
+pick_T(emulo,ix,iy,iz,elo);
+ldouble pp[NV],T;
+
+/*****************************/
+
+rho=RADBEAMFLAT_RHO;
+uint=RADBEAMFLAT_UU;
+
+  E= RADBEAMFLAT_ERAD;
+Fx=Fy=Fz=0.*E;
+
+
+pp[0]=rho;
+pp[1]=uint;
+pp[2]=0.;
+pp[3]=0.;
+pp[4]=0.;
+pp[5]=calc_Sfromu(rho,uint);
+pp[6]=E;
+pp[7]=Fx;
+pp[8]=Fy;
+pp[9]=Fz;
+
+prad_zamo2ff(pp,pp,gg,GG,eup);
+prad_ff2lab(pp,pp,gg,GG,tlo);
+
+p2u(pp,uu,gg,GG);
+
+
+
+/**************************/
+
+int iv;
+
+for(iv=0;iv<NV;iv++)
+  {
+    set_u(u,iv,ix,iy,iz,uu[iv]);
+    set_u(p,iv,ix,iy,iz,pp[iv]);
+  }
+
+//entropy
+update_entropy(ix,iy,iz,0);
+
+//if(isnan(get_u(p,5,ix,iy,iz))) {printf("pr: %d %d %d S: %Le\n",ix,iy,iz,0.);getchar();}
+
+//mark initialy succesfull u2p_hot step
+set_cflag(0,ix,iy,iz,0);
