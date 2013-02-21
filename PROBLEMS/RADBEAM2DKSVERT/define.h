07b455cfef5da266ea5f4e85f9fff5391ed80092
Olek <olek.sadowski@gmail.com>
2013-02-20 20:15:09 -0500

    Working on it

--- PROBLEMS/RADBEAM2DKSVERT/define.h
+++ PROBLEMS/RADBEAM2DKSVERT/define.h
@@ -1,5 +1,4 @@
 #define TMAX 1.e10
-//#define FLUXDISSIPATIONFULL
 #define RADIATION
 //#define myMKS1COORDS
 
@@ -13,8 +12,8 @@
 #define OUTCOORDS KERRCOORDS //coordinates for output
 
 #define NX 30
-#define NY 1
-#define NZ 30
+#define NY 30
+#define NZ 1
 #define TSTEPLIM .5//kind of courant limiter
 #define INT_ORDER 1
 #define RK3STEPPING
@@ -68,8 +67,8 @@
 #define DTOUT1 .25 //dt for basic output
 #endif
 
-#define MINY .99*Pi/2.
-#define MAXY 1.01*Pi/2.
+#define MINY .5*Pi/2.
+#define MAXY Pi/2.
 
 #define MINZ 0.
 #define MAXZ Pi/4.//8.*Pi/4.
@@ -88,18 +87,11 @@
 #define RHOAMB 1.e0
 #define KAPPA 0.
 #define SPECIFIC_BC
-#define COPY_XBC
-#define COPY_YBC
-#define COPY_ZBC
-#define YZXDUMP
+//#define YZXDUMP
 #define RADOUTPUTINZAMO
 //#define PRINTGC_LEFT
 //#define PRINTGC_RIGHT
-//#define BLOB
-#define BLOBW .1
-#define BLOBP 100000.
-#define BLOBX 10.
-#define BLOBZ Pi/20.
+
 #define U2PPREC 1.e-6
 #define U2PRADPREC 1.e-4
 #define RADFORCEPREC 1.e-5
