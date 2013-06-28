deff9c19eb4ac857d7fd4d6e4aa68c3eef5b9a3c
Olek <asadowski@cfa.harvard.edu>
2013-06-28 09:03:31 -0400

    Corrected parallelization of ifingr

--- PROBLEMS/G2STATIC/finger.c
+++ PROBLEMS/G2STATIC/finger.c
@@ -4,34 +4,38 @@
 #ifdef TRACER
 
 int ix,iy,iz,ii,iv;
-ldouble trace,rhoorg;
-ldouble pp[NV],uu[NV];
 /**************************/
 
 #pragma omp parallel for private(ix,iy,iz,iv) schedule (dynamic)
 for(ii=0;ii<Nloop_0;ii++) //domain only
   {
+    ldouble trace,f;
+    ldouble pp[NV],ppinit[NV],uu[NV];
+    struct geometry geom;
     ix=loop_0[ii][0];
     iy=loop_0[ii][1];
     iz=loop_0[ii][2]; 
  
     trace=get_u(p,TRA,ix,iy,iz);
-    rhoorg=get_u(p,0,ix,iy,iz);
 
     if(trace<MINTRACE)
       {
-	struct geometry geom;
 	fill_geometry(ix,iy,iz,&geom);
+	f = trace / MINTRACE;
 	for(iv=0;iv<NV;iv++)
 	  {
-	    pp[iv]=get_u(pinit,iv,ix,iy,iz);	
+	    ppinit[iv]=get_u(pinit,iv,ix,iy,iz);	
+	    pp[iv]=get_u(p,iv,ix,iy,iz);
+
+	    if(f<0. || f>1.) {printf("wrong f %e %e %e\n",f,trace,MINTRACE);getchar();}
+	    pp[iv]=ppinit[iv]*(1.-f) + pp[iv]*f;
 	  }
 	//hd floors
 	check_floors_hd(pp,VELPRIM,&geom);
 	p2u(pp,uu,&geom);
 	for(iv=0;iv<NV;iv++)
 	  {
-	    if(iv==TRA) continue;
+	    if(iv==TRA) continue; //do not overwrite the tracer field
 	    set_u(u,iv,ix,iy,iz,uu[iv]);
 	    set_u(p,iv,ix,iy,iz,pp[iv]);
 	  }
