5438a604806d700c8882d8e15ba6e16ebb6317b5
Olololo <olek.sadowski@gmail.com>
2013-08-01 13:56:26 -0400

    ORSZAG hd

--- PROBLEMS/ORSZAG/out2gif_2d.c
+++ PROBLEMS/ORSZAG/out2gif_2d.c
@@ -1,179 +1,128 @@
-//int
-//convert_out2gif_2d(char *fname,char *fname2,int niter,ldouble t)
-//{
-//  FILE *fgnu=fopen("plot.gp","w");
-//  char bufor[50];
-
-ldouble minx,miny,maxx,maxy;
-
-#if (OUTCOORDS==KERRCOORDS)
-minx= -.02*(exp(get_xb(-NG,0))+MKS1R0);
-maxx= 1.01*(exp(get_xb(NX,0))+MKS1R0);
-miny= -.02*(exp(get_xb(-NG,0))+MKS1R0);
-maxy= 1.01*(exp(get_xb(NX,0))+MKS1R0);
-
-//maxx= 25.;
-//maxy= 25.;
-#else
-minx= -.02*get_xb(NX,0);
-maxx= 1.02*get_xb(NX,0);
-miny= -.02*get_xb(NX,0);
-maxy= 1.02*get_xb(NX,0);
-#endif
-
   fprintf(fgnu,
-"set table \"table.gp\"\n"
+          
+	  "set table \"table.gp\"\n"
 	  "set contour base\n"
 	  "unset surface\n"
 	  "set log z\n"
-	  "set cntrparam levels discrete 1 \n"
-	  "splot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):25 w l\n"
+	  "set cntrparam levels discrete 1.1,3,6,10,30,60,100 \n"
+	  "splot \"%s\" u 1:2:14 w l\n"
 	  "unset dgrid3d\n"
 	  "unset log z\n"
 	  "unset table\n"
 	  "unset contour\n"
 	  "unset surface\n"
 
-	  "set term gif large size 1100,500\n"
+#ifndef MAGNFIELD
+	  "set term gif large size 800,700\n"
+#else
+	  "set term gif large size 1600,700\n"
+#endif
 	  "set output \"%s\"\n"
+	  "set view map\n"
+	  "unset surface\n"
 	  "set size 1,1\n"
 	  "set origin 0,0\n"
 	  "set multiplot\n"
-
-	  "set view map\n"
 	  "set pm3d\n"
-	  "unset surface\n"
 	  "unset key\n"
-	  "set style line 1 lt 1 lw 2 lc 3\n"
-	  "set style line 11 lt 1 lw 2 lc 6\n"
-	  "set style line 2 lt 1 lw 2 lc 2\n"
-	  "set style line 3 lt 1 lw 2 lc 2\n"
-	  "set style line 21 lt 3 lw 1 lc -1\n"
-	 
-	  "set style arrow 1 ls 2\n"
-	  
+	  "set style line 1 lt 1 lw 2 lc 2\n"
+	  "set style line 2 lt 1 lw 3 lc 3\n"
+	  "set style arrow 1 head nofilled size screen 0.002,35 ls 1\n"
+	  "set style arrow 1 ls 1\n"
 	  "set palette model RGB rgbformulae 7,5,15\n"
-	  /*
 	  "set palette model RGB rgbformulae 30,31,32\n"
 	  "set palette model RGB rgbformulae 21,22,23\n"
-	  "set palette model RGB rgbformulae 23,28,3\n"
-	  "set palette model RGB rgbformulae 7,8,9\n"
-	  //"set palette model RGB rgbformulae 35,3,9\n"
-	  //	  "set palette model RGB rgbformulae 6,3,21\n"
-	  */
-	  "set autoscale\n"
+	  "set palette model RGB rgbformulae 21,22,23\n"
+	  "set palette model RGB rgbformulae 35,3,9\n"
+	  "set palette model RGB rgbformulae 21,22,23\n"
+
+#ifndef MAGNFIELD
+	  "set lmargin at screen 0.07\n"
+	  "set rmargin at screen 0.85\n"
+#else
+	  "set lmargin at screen 0.05\n"
+	  "set rmargin at screen 0.425\n"
+#endif
+	  "set bmargin at screen .12\n"
+	  "set tmargin at screen .95\n"
+	  "set autoscale \n"
 	  "set xrange [%f:%f]\n"
 	  "set yrange [%f:%f]\n"
+	  //"set cbrange [1:40]\n"
+	  //"set log cb\n"
 
-	  "set lmargin at screen 0.04\n"
-	  "set rmargin at screen 0.41\n"
-	  "set bmargin at screen .10\n"
-	  "set tmargin at screen .95\n"
-	  "set log cb\n"
-	  "set ylabel \"z\"\n"
-	  "set xlabel \"x\" offset 0,1\n"
+	  "set xlabel \"x\"\n"
+	  "set ylabel \"y\"\n"
 	  "set cblabel \"\"\n"
-	  "set title \"rho\" offset 0,-1\n"
-	  "set format cb \"%%.1e\"\n"
-#ifndef CBAUTOSCALE
-	  "set cbrange [1e-26:1e-19]\n"
-#endif
-	  
-	  "splot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):($14) ti \"\" w l ls 1\n"
-
+	  "set title \"hydro\" offset 0,-1\n"
+	  "splot \"%s\" u 1:2:($14) w l ti \"\"\n"
+	 
+	  "unset pm3d\n"
 	  "set isosam 10,10\n"
-	  "set ylabel \"\"\n"
+	  "set format x \"\"\n"
+	  "set format y \"\"\n" 
 	  "set xlabel \"\"\n"
-	  "unset tics\n"
-	  "unset title\n"
-	  "unset border\n"
-	  "unset log cb\n"
-
-	  "plot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):"
-	  "(($16*sin($2)+$17*cos($2))*%f):"
-	  "((-$17*sin($2)+$16*cos($2))*%f) every %d:%d w vectors arrowstyle 1 ti \"\"\n"
+	  "set ylabel \"\"\n"
+	  "set title \"\" offset 0,-1\n"
+	  "plot \"%s\" u 1:2:(($16)"
+	  "/((($16)*($16)+($17)*($17))**.5)/%f):"
+	  "(($17)/((($16)*($16)+($17)*($17))**.5)/%f)"
+	  "every %d:%d w vectors arrowstyle 1 ti \"\"\n"
 
 	  "unset tics\n"
 	  "unset border\n"
 	  "unset pm3d\n"
 	  "unset surface\n"
-	  "plot \"table.gp\" w l ls 1\n"
+	  "plot \"table.gp\" w l ls 2\n"
 	  "set pm3d\n"
 	  "set tics\n"
 	  "set border\n"
 
-	  "set border\n"
-	  "set tics\n"
-	  "set lmargin at screen 0.53\n"
+#ifdef MAGNFIELD
+	  "set lmargin at screen 0.525\n"
 	  "set rmargin at screen 0.90\n"
-	  "set bmargin at screen .10\n"
+	  "set bmargin at screen .12\n"
 	  "set tmargin at screen .95\n"
-	  "set log cb\n"
-	  "set xlabel \"x\" offset 0,1\n"
-	  "set ylabel \"z\"\n"
+	  "set autoscale \n"
+	  "set xrange [%f:%f]\n"
+	  "set yrange [%f:%f]\n"
+	  //"set cbrange [1:40]\n"
+	  //"set log cb\n"
+
+	  "set xlabel \"x\"\n"
+	  "set ylabel \"y\"\n"
 	  "set cblabel \"\"\n"
-#ifndef CBAUTOSCALE
-	  "set cbrange [1.e-26:1.e-22]\n"
-#endif
-	  //"set autoscale cb\n"
-
-#ifdef RADIATION	  
-	  "set title \"E\" offset 0,-1\n"
-	  "splot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):($20) ti \"\" w l ls 1\n"
-#else
-	  "set title \"u_int\" offset 0,-1\n"
-	  "splot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):($15) ti \"\" w l ls 1\n"
-#endif
-
+	  "set title \"mag\" offset 0,-1\n"
+	  "splot \"%s\" u 1:2:($20**2+$21**2+$22**2)**.5 w l ti \"\"\n"
+	 
+	  "unset pm3d\n"
 	  "set isosam 10,10\n"
-	  "set ylabel \"\"\n"
+	  "set format x \"\"\n"
+	  "set format y \"\"\n" 
 	  "set xlabel \"\"\n"
-	  "unset tics\n"
-	  "unset title\n"
-	  "unset border\n"
-	  "unset log cb\n"
-#ifdef RADIATION	  
-	  "plot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):"
-	  "(($21*sin($2)+$22*cos($2))/(($21*sin($2)+$22*cos($2))**2+(-$22*sin($2)+$21*cos($2))**2)**.5*%f):"
-	  "((-$22*sin($2)+$21*cos($2))/(($21*sin($2)+$22*cos($2))**2+(-$22*sin($2)+$21*cos($2))**2)**.5*%f) every %d:%d w vectors arrowstyle 1 ti \"\"\n"
-#else
-	  "plot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):"
-	  "(($16*sin($2)+$17*cos($2))*%f):"
-	  "((-$17*sin($2)+$16*cos($2))*%f) every %d:%d w vectors arrowstyle 1 ti \"\"\n"
-#endif
-	  //	  "plot \"%s\" u (($1)*sin($2)):(($1)*cos($2)):"
-	  //	  "(($21*sin($2)+$22*cos($2))*%f):"
-	  //	  "((-$22*sin($2)+$21*cos($2))*%f) every %d:%d w vectors arrowstyle 1 ti \"\"\n"
+	  "set ylabel \"\"\n"
+	  "set title \"\" offset 0,-1\n"
+	  "plot \"%s\" u 1:2:(($20)"
+	  "/((($20)*($20)+($21)*($21))**.5)/%f):"
+	  "(($21)/((($20)*($20)+($21)*($21))**.5)/%f)"
+	  "every %d:%d w vectors arrowstyle 1 ti \"\"\n"
 
 	  "unset tics\n"
 	  "unset border\n"
 	  "unset pm3d\n"
 	  "unset surface\n"
-	  "plot \"table.gp\" w l ls 1\n"
+	  "plot \"table.gp\" w l ls 2\n"
 	  "set pm3d\n"
 	  "set tics\n"
 	  "set border\n"
-	  ,fname
- 	  ,fname2,
-	  minx,
-	  maxx,
-	  miny,
-	  maxy,
-	  fname,
-	  fname,5.,5.,NX/21+1,NY/21+1,
-	  fname,
-#ifdef RADIATION
-	  fname,1.,1.,NX/21+1,NY/21+1
-#else
-	  fname,3.,3.,NX/21+1,NY/21+1
 #endif
-	  );  
-//#endif	    
 
-/*
-  fprintf(fgnu,"\n");
-  fclose(fgnu);   
-  
-  int i=system("gnuplot plot.gp ");
-  return 0;
-*/
+	  ,fname,fname2,get_xb(0,0),get_xb(NX,0),get_xb(0,1),get_xb(NY,1)
+	  ,fname,fname,50./(get_xb(NX,0)-get_xb(0,0)),50./(get_xb(NY,1)-get_xb(0,1)),(int)(NX/20),(int)(NY/20)
+#ifdef MAGNFIELD
+	  ,get_xb(0,0),get_xb(NX,0),get_xb(0,1),get_xb(NY,1)
+	  ,fname,fname,50./(get_xb(NX,0)-get_xb(0,0)),50./(get_xb(NY,1)-get_xb(0,1)),(int)(NX/20),(int)(NY/20)
+#endif
+);
+	    
+	
