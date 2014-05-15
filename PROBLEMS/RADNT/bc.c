eb8c7515281991744d26c19212c6d9209c6bacb9
Olololo <olek.sadowski@gmail.com>
2014-05-15 14:56:00 -0400

    nic

--- PROBLEMS/RADNT/bc.c
+++ PROBLEMS/RADNT/bc.c
@@ -202,15 +202,14 @@ if(iy>=NY) //equatorial plane
       {
 
 	pp[6]=calc_LTE_EfromT(1.e11)*(1.-sqrt(rin/rBL))/pow(rBL,3.);
-	pp[7]=pp[8]=pp[9]=0.;
-	pp[8]=0.*pp[6];
+	pp[7]=pp[9]=0.;
+	pp[FY0]=-0.5*pp[6];
 
 	//Keplerian gas
 	ldouble Om=1./pow(rBL,1.5)*OMSCALE;
 	
-	//ldouble ucon[4]={0.,0.,-0.5/rBL,Om};
 	ldouble ucon[4]={0.,0.,0.,Om};
-
+	
 	conv_vels(ucon,ucon,VEL3,VELPRIM,ggBL,GGBL);
 		
 	pp[2]=ucon[1];
